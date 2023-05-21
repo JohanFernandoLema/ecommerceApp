d833f6f0a934372dbf2313e7353dc92a128b9706
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-21 13:06:22 -0400

    commerce API loaded by getting pk from commerce.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './index'
+
+import { commerce } from './lib/commerce'
 import { Navbar } from './components'
 import { Products } from './components/products/Products'
 
