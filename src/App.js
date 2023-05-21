7a394f3c4bc4c07e4e40001a93ae6ef47451b979
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-21 12:36:32 -0400

    fontawesome dependencies added in order to get acces to more icons, navbar structure created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Products } from './components/products/Products'
+import Navbar from './components/navbar/navBar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   )
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 import Product from './product/Product'
+import useStyles from './styles'
 
 const products = [
   {
@@ -20,8 +21,10 @@ const products = [
 ]
 
 export const Products = () => {
+  const classes = useStyles()
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justifyContent="center" spacing={4}>
         {products.map((product) => {
           return (
